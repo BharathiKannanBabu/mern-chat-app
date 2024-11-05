9fad91ea42c621799e0321bd43bdfddf5409909c
BharathiKannanBabu <bharathikannan130552055@gmail.com>
2024-11-05 18:18:06 +0530

    Deplayment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
-import  express  from "express";
+import path  from "path";
+import express  from "express";
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
@@ -11,14 +12,19 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 dotenv.config();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-// app.get("/", (req, res) => {
-//     res.send("Hello world!")
-// })
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
